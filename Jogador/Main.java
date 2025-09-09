9ff69395f8fc8d3cd8de714323c479c563edaae4
Gustavo Zembruski <151480220+goolgeima@users.noreply.github.com>
2025-09-09 20:12:02 -0300

    Add files via upload

---
+++ Jogador/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        Jogador jogador = new Jogador();
+
+        System.out.println("Digite qual ano você nasceu: ");
+        jogador.anoNascimento = sc.nextInt();
+
+        System.out.println("Digite qual sua posição: (1- Atacante, 2- Meio-campo, 3- Defesa)");
+        jogador.Posicao = sc.nextInt();
+
+        System.out.println("Sua idade é: " + jogador.idade());
+
+        System.out.println("Você ainda tem " + jogador.faltaAposentar() + " anos de carreira");
+    }
+}
